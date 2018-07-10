6ee5461b2a5bcb24877a5772e6059a0c6068dd2f
Nathan Lin <nlin@theticker.org>
2018-07-10 21:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function logShout(string) {
+function shout(string) {
   return string.toUpperCase()
 }
 
 function whisper(string) {
   return string.toLowerCase() 
 }
+
+function logShout(string) {
+  
+} 
+
+
 
 function sayHiToGrandma(string) {
   return "I can/'t hear you!" 
